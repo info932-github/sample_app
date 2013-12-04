03b2dff2d1ec6ec6ce32e5f52756f2f1a65a706f
Jeff Wise <jwise@wgen.com>
2013-12-04 12:57:52 -0500

    finish 10.3.3

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -10,6 +10,7 @@ class MicropostsController < ApplicationController
   		flash[:success] = "micropost created!"
   		redirect_to root_url
   	else
+      @feed_items = []
   		render 'static_pages/home'
   	end
   end
